bbacf692af96a125aeb21d6f91a96b9619a34d80
Tadeo Vazquez <tadepapusio2012@gmail.com>
2022-07-21 00:10:02 -0400

    All Done con Verificaciones de CAIDO y NO HAY

--- lib/cita/screens/lista_todas_citas.dart
+++ lib/cita/screens/lista_todas_citas.dart
@@ -44,14 +44,18 @@ class _todasCitasListaState extends State<todasCitasLista> {
         FutureBuilder(
           future: ServiceCitaApi.getTodasCitas(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.none:
-                return const Center(child: Text('NONE'));
-              case ConnectionState.active:
-              case ConnectionState.done:
-                return _ListaDeCitas(snapshot.data);
+            if (snapshot.hasData) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.none:
+                  return const Center(child: Text('NONE'));
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  return _ListaDeCitas(snapshot.data);
+              }
+            } else {
+              return const Center(child: Text('SERVIDOR CAIDO'));
             }
           },
         )
@@ -61,31 +65,35 @@ class _todasCitasListaState extends State<todasCitasLista> {
 
   Widget _ListaDeCitas(dynamic data) {
     return Expanded(
-        child: ListView.builder(
-      itemCount: data.length,
-      itemBuilder: (context, index) {
-        final cita = data[index];
-        return Card(
-            child: Container(
-          padding: const EdgeInsets.all(50.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                child: CampoCita(
-                    dato:
-                        cita.paciente.pNombre + ' ' + cita.paciente.pApellido),
-              ),
-              Expanded(child: CampoCita(dato: cita.fechacita)),
-              Expanded(child: CampoCita(dato: cita.horacita)),
-              Expanded(child: CampoCita(dato: cita.modalidad)),
-              Expanded(child: CampoCita(dato: cita.statuscita)),
-              if (cita.statuscita == "Aceptada") _mostrarBotones(cita.idCita)
-            ],
-          ),
-        ));
-      },
-    ));
+        child: data.length >= 1
+            ? ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  final cita = data[index];
+                  return Card(
+                      child: Container(
+                    padding: const EdgeInsets.all(50.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: CampoCita(
+                              dato: cita.paciente.pNombre +
+                                  ' ' +
+                                  cita.paciente.pApellido),
+                        ),
+                        Expanded(child: CampoCita(dato: cita.fechacita)),
+                        Expanded(child: CampoCita(dato: cita.horacita)),
+                        Expanded(child: CampoCita(dato: cita.modalidad)),
+                        Expanded(child: CampoCita(dato: cita.statuscita)),
+                        if (cita.statuscita == "Aceptada")
+                          _mostrarBotones(cita.idCita)
+                      ],
+                    ),
+                  ));
+                },
+              )
+            : const Center(child: Text('NO HAY CITAS ACTUALMENTE')));
   }
 
   Widget _mostrarBotones(String idCita) {
